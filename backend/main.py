32cb72ba2c6df53fad362ef197b0f03dc0fc28c1
Nerdo Pulido <nerdop@gmail.com>
2026-02-16 12:05:07 -0400

    Deploy: Direct IP Mode for TV (v1.4) and open CORS

--- backend/main.py
+++ backend/main.py
@@ -247,7 +247,7 @@ allowed_origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
